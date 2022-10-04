6eb480f67159a47239f11de9c53c2c4685cf13b1
elye <elye.project@gmail.com>
2022-10-04 23:01:57 +1100

    Update to https://stackoverflow.com/q/73947154/3286489 to fix the issue compose 1.1.1 version issue, and also https://stackoverflow.com/q/73947501/3286489

--- app/src/main/java/com/example/learnabstractcomposeview/MyComposeView.kt
+++ app/src/main/java/com/example/learnabstractcomposeview/MyComposeView.kt
@@ -11,7 +11,8 @@ import androidx.compose.ui.R
 import androidx.compose.ui.platform.AbstractComposeView
 import androidx.lifecycle.ViewTreeLifecycleOwner
 import androidx.lifecycle.ViewTreeViewModelStoreOwner
-import androidx.savedstate.ViewTreeSavedStateRegistryOwner
+import androidx.savedstate.findViewTreeSavedStateRegistryOwner
+import androidx.savedstate.setViewTreeSavedStateRegistryOwner
 import java.util.*
 
 @SuppressLint("ViewConstructor")
@@ -38,7 +39,7 @@ class MyComposeView(
     init {
         ViewTreeLifecycleOwner.set(this, ViewTreeLifecycleOwner.get(composeView))
         ViewTreeViewModelStoreOwner.set(this, ViewTreeViewModelStoreOwner.get(composeView))
-        ViewTreeSavedStateRegistryOwner.set(this, ViewTreeSavedStateRegistryOwner.get(composeView))
+        setViewTreeSavedStateRegistryOwner(composeView.findViewTreeSavedStateRegistryOwner())
         saveID?.let {
             setTag(R.id.compose_view_saveable_id_tag, "MyComposeView:$saveID")
         }
@@ -78,7 +79,7 @@ class MyComposeView(
 
     fun dispose() {
         dismiss()
-        ViewTreeSavedStateRegistryOwner.set(this, null)
+        setViewTreeSavedStateRegistryOwner(null)
         ViewTreeLifecycleOwner.set(this, null)
         ViewTreeViewModelStoreOwner.set(this, null)
     }
